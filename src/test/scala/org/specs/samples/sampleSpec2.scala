edf12cfb1027d42733f6557946360076879c4691
etorreborre <etorreborre@yahoo.com>
2008-07-21 11:10:30 +0900

    tested the tags with ScalaTest -> ok

--- src/test/scala/org/specs/samples/sampleSpec2.scala
+++ src/test/scala/org/specs/samples/sampleSpec2.scala
@@ -10,14 +10,14 @@ object sampleSpec2 extends Specification {
   }
 }
 import org.specs.runner._
-class helloWorldTest extends JUnit4(helloWorld)
+class helloWorldTest extends Runner(helloWorld) with JUnit with ScalaTest
 object helloWorld extends Specification("Hello world") {
   "'hello world' has 11 characters" in {
      "hello world".size mustBe 11
   }
   "'hello world' matches 'h.* w.*'" in {
     "hello world" must beMatching("h.* w.*")
-  }
+  } tag "excluded"
 }
 import org.specs.mock._
 import java.io._
