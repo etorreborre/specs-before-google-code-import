44be22de371d6a6026803c7f38825b43c6a9bb4e
etorreborre <etorreborre@yahoo.com>
2008-10-25 10:05:31 +0100

    added forms as linkable objects

--- src/test/scala/org/specs/util/formSpec.scala
+++ src/test/scala/org/specs/util/formSpec.scala
@@ -18,17 +18,35 @@ object formSpec extends Specification with Forms {
                             "  First Name: Eric\n" +
                             "  Last Name: Torreborre"
     }
+    "have a toString method displaying following properties" in {
+      val form = new Person("Person") {
+        firstName("Eric") --> lastName("Torreborre")
+      }
+      form.toString must_== "Person\n" +
+                            "  First Name: Eric, Last Name: Torreborre"
+    }
+    "have a toString method printing properties on the same line if they are in sequence" in {
+      val form = new Person("Person") {
+        firstName("Eric") --> lastName("Torreborre")
+      }
+      form.toString must_== "Person\n" +
+                            "  First Name: Eric, Last Name: Torreborre"
+    }
   }
   "A form property" should {
     "have a toString method returning the name and the property value" in {
       Prop("Name", "Eric").toString must_== "Name: Eric"
     }
-    "have a follows property giving an order to properties" in {
-      val form = new Person("Person") {
-        firstName("Eric") -> lastName("Torreborre")
-      }
-      form.toString must_== "Person\n" +
-                            "  First Name: Eric, Last Name: Torreborre"
+    "have next properties" in {
+      val firstName = Prop("First Name", "Eric")
+      firstName --> (Prop("Last Name", "Torreborre"))
+      firstName.toString must_== "First Name: Eric, Last Name: Torreborre"
+    }
+    "have a previous property" in {
+      val firstName = Prop("First Name", "Eric")
+      val lastName = Prop("Last Name", "Torreborre")
+      firstName --> lastName
+      lastName.previous must be_==(Some(firstName))
     }
   }
   "A form" can {
@@ -37,31 +55,71 @@ object formSpec extends Specification with Forms {
         firstName("Eric")
       }
       form.firstName.label must_== "First Name"
-      form.firstName() must_== "Eric"
+      form.firstName.get must_== "Eric"
+    }
+    "embedded another following form as if it was a property" in {
+      val form = new Person("person") {
+        firstName("Eric") --> new Address("home") {
+                                 number(37)
+                                 street("Nando-cho")
+                              }	 
+      }
+      detailedDiffs()
+      form.toString must_== "person\n" +
+                            "  First Name: Eric, home\n" +
+                            "  Number: 37\n" +
+                            "  Street: Nando-cho\n" +
+                            "  Last Name: "
     }
   }
   case class Person(t: String) extends Form(t) with Properties {
     val firstName: Prop[String] = prop("First Name", "")
     val lastName: Prop[String] = prop("Last Name", "")
   }
+  case class Address(t: String) extends Form(t) with Properties {
+    val number: Prop[Int] = prop("Number", 1)
+    val street: Prop[String] = prop("Street", "")
+  }
   
 }
 trait Forms {
-  case class Prop[T](label: String, value: T) extends org.specs.util.Property(value) {
-    override def toString = label + ": " + this()
+  trait Linkable {
+    val next: ListBuffer[Linkable] = new ListBuffer() 
+    var previous: Option[Linkable] = None
+    def -->(others: Linkable*) = {
+      next.appendAll(others)
+      others.foreach(_.previous = Some(this))
+    }
   }
-  case class Form(title: String) {
-    def prop[T](label: String, defaultValue: T) = { 
-      val p = Prop(label, defaultValue)
+  case object Prop {
+    def apply[T](label: String, value: T) = new Prop(label, Some(value))
+  }
+  case class Prop[T](label: String, value: Option[T]) extends Property(value) with Linkable {
+   
+    def apply(v: T) = { super.apply(Some(v)); this }
+    def get: T = this().get
+    override def toString = {
+      label + ": " + this().getOrElse("_") + 
+      (if (next.isEmpty) "" else ", ") +
+      next.toList.mkString(", ")
+    }
+  }
+  case class Form(title: String) extends Linkable {
+    protected val properties: ListBuffer[Prop[_]] = new ListBuffer
+    def prop[T](label: String) = {
+      val p = Prop(label, None)
       properties.append(p)
       p
     } 
-    implicit def toSequence[T](prop: Prop[T]) = new Object {
-      def ->[U](other: Prop[U]) = propertiesConstraints.put(prop, other)
-    }
-    protected val properties: ListBuffer[Prop[_]] = new ListBuffer
-    protected val propertiesConstraints: scala.collection.mutable.HashMap[Prop[_], Prop[_]] = new scala.collection.mutable.HashMap[Prop[_], Prop[_]]
-    override def toString = title + properties.mkString("\n  ", "\n  ", "") 
+    def prop[T](label: String, defaultValue: T) = { 
+      val p = Prop(label, Some(defaultValue))
+      properties.append(p)
+      p
+    } 
+    override def toString = {
+      title + 
+      properties.filter(_.previous.isEmpty).mkString("\n  ", "\n  ", "")
+    } 
   }
 }
 class formTest extends org.specs.runner.JUnit4(formSpec)
