51ed2f7f5048f70fd5efddbed1b29acb7a3d9be8
etorreborre <etorreborre@yahoo.com>
2009-03-02 09:49:39 +0900

    added a println to diagnose the issue

--- src/main/scala/org/specs/runner/JUnit.scala
+++ src/main/scala/org/specs/runner/JUnit.scala
@@ -7,20 +7,20 @@ import org.specs.util.Classes
 
 /**
  * The strategy for running Specifications with JUnit is as follow:<p>
- * 
+ *
  * -the specifications are used to create JUnit3 TestSuite and TestCase objects
- * -however, since TestSuite is not an interface, a JUnitSuite trait is used to represent it (and the JUnitSuite uses a TestSuite internally to hold the tests) 
+ * -however, since TestSuite is not an interface, a JUnitSuite trait is used to represent it (and the JUnitSuite uses a TestSuite internally to hold the tests)
  *   -a specification is represented as a JUnitSuite
  *   -a system under test (sus) is represented as an ExamplesTestSuite <: JUnitSuite
  *   -an example is represented as an ExampleTestCase <: TestCase
- * 
- * Then, the JUnitSuite which implements the junit.framework.Test interface can be run using 
+ *
+ * Then, the JUnitSuite which implements the junit.framework.Test interface can be run using
  * a custom JUnit4 annotation: JUnitSuiteRunner
- * 
+ *
  * This JUnitSuite trait encapsulates a JUnit3 TestSuite to allow it being seen as a trait and not a class
  * The suite is initialized once whenever some information is required, like getName, or countTestCases
  */
-trait JUnitSuite extends Test { 
+trait JUnitSuite extends Test {
   /** embedded JUnit3 TestSuite object */
   val testSuite = new TestSuite
 
@@ -34,7 +34,7 @@ trait JUnitSuite extends Test {
   def initialize
 
   /** run the embedded suite */
-  def run(result: TestResult) = {init; testSuite.run(result)} 
+  def run(result: TestResult) = {init; testSuite.run(result)}
 
   /** @return the name of the embedded suite */
   def getName = {init; testSuite.getName}
@@ -60,7 +60,7 @@ trait JUnitSuite extends Test {
 
   /** @return the test suites of the embedded suite */
   def suites = {
-    init 
+    init
     for (ts <- tests; if (ts.isInstanceOf[JUnitSuite] || ts.isInstanceOf[TestSuite]))
          yield ts.asInstanceOf[Test]
   }
@@ -70,12 +70,12 @@ trait JUnitSuite extends Test {
  * Extension of a JUnitSuite initializing the suite with one or more specifications
  */
 trait JUnit extends JUnitSuite with Reporter {
-  def initialize = { 
+  def initialize = {
     if (specs.size > 1)
       setName(this.getClass.getName.replaceAll("\\$", ""))
     else
 	   setName(specs(0).description)
-	specs foreach { specification => 
+	specs foreach { specification =>
 	  specification.subSpecifications.foreach {s: Specification => addTest(new JUnit3(s))}
 	  specification.systems foreach {sus => addTest(new ExamplesTestSuite(sus.description + " " + sus.verb, sus.examples, sus.skippedSus))}
 	}
@@ -87,21 +87,21 @@ trait JUnit extends JUnitSuite with Reporter {
  * @deprecated use JUnit4 instead as the JUnitSuite class runs with a JUnit4 runner
  */
 @RunWith(classOf[JUnitSuiteRunner])
-class JUnit3(val specifications : Specification*) extends JUnit { 
-  val specs: Seq[Specification] = specifications 
+class JUnit3(val specifications : Specification*) extends JUnit {
+  val specs: Seq[Specification] = specifications
 }
 
 /**
  * Concrete class providing specifications to be used as a JUnitSuite
  */
 @RunWith(classOf[JUnitSuiteRunner])
-class JUnit4(val specifications : Specification*) extends JUnit { 
-  val specs: Seq[Specification] = specifications 
+class JUnit4(val specifications : Specification*) extends JUnit {
+  val specs: Seq[Specification] = specifications
 }
 
 
 /**
- * A <code>ExamplesTestSuite</code> is a JUnitSuite reporting the results of 
+ * A <code>ExamplesTestSuite</code> is a JUnitSuite reporting the results of
  * a System under test (sus) as a list of examples, represented by ExampleTestCase objects. If an example has subExamples, they are reported with a separate <code>ExamplesTestSuite</code>
  */
 class ExamplesTestSuite(description: String, examples: Iterable[Example], skipped: Option[Throwable]) extends JUnitSuite with Classes {
@@ -114,10 +114,10 @@ class ExamplesTestSuite(description: String, examples: Iterable[Example], skippe
   def initialize = {
     setName(description)
     examples foreach { example =>
-      // if the test is run with Maven the sus description is added to the example description for a better 
+      // if the test is run with Maven the sus description is added to the example description for a better
       // description in the console
       val exampleDescription = (if (isExecutedFromMaven) (description + " ") else "") + example.description
-      
+
       if (example.subExamples.isEmpty)
         addTest(new ExampleTestCase(example, exampleDescription))
       else
@@ -126,7 +126,7 @@ class ExamplesTestSuite(description: String, examples: Iterable[Example], skippe
   }
 
   /**
-   * runs the set of exemplesA <code>ExamplesTestSuite</code> is a JUnitSuite reporting the results of 
+   * runs the set of exemplesA <code>ExamplesTestSuite</code> is a JUnitSuite reporting the results of
    * a list of examples. If an example has subExamples, they are reported with a separate <code>ExamplesTestSuite</code>
    * If the list of examples is skipped (because the corresponding sus is skipped), then a SkippedAssertionError failure is sent
    * and the JUnitSuiteRunner will interpret this as an ignored test (this functionality wasn't available in JUnit3)
@@ -138,21 +138,22 @@ class ExamplesTestSuite(description: String, examples: Iterable[Example], skippe
 }
 
 /**
- * A <code>ExampleTestCase</code> reports the result of an example<p> 
+ * A <code>ExampleTestCase</code> reports the result of an example<p>
  * It overrides the run method from <code>junit.framework.TestCase</code>
  * to add errors and failures to a <code>junit.framework.TestResult</code> object
  */
-class ExampleTestCase(example: Example, description: String) extends TestCase(description.replaceAll("\n", " ")) { 
+class ExampleTestCase(example: Example, description: String) extends TestCase(description.replaceAll("\n", " ")) {
   override def run(result: TestResult) = {
-      result.startTest(this)
-      example.failures foreach {failure: FailureException => result.addFailure(this, new SpecAssertionFailedError(failure))}
-      example.skipped foreach {skipped: SkippedException => result.addFailure(this, new SkippedAssertionError(skipped)) }
-      example.errors foreach {error: Throwable => result.addError(this, new SpecError(error)) }
-      result.endTest(this)
+    result.startTest(this)
+    println("running test: " + description)
+    example.failures foreach {failure: FailureException => result.addFailure(this, new SpecAssertionFailedError(failure))}
+    example.skipped foreach {skipped: SkippedException => result.addFailure(this, new SkippedAssertionError(skipped)) }
+    example.errors foreach {error: Throwable => result.addError(this, new SpecError(error)) }
+    result.endTest(this)
   }
 }
 /**
- * This class refines the <code>AssertionFailedError</code> from junit 
+ * This class refines the <code>AssertionFailedError</code> from junit
  * and provides the stackTrace of an exception which occured during the specification execution
  */
 class SpecAssertionFailedError(t: Throwable) extends AssertionFailedError(t.getMessage){
--- src/main/scala/org/specs/runner/JUnitSuiteRunner.scala
+++ src/main/scala/org/specs/runner/JUnitSuiteRunner.scala
@@ -13,19 +13,19 @@ import org.specs.util.Classes
  * <code>klass</code> is a JUnitSuite which implements the junit.framework.Test interface
  */
 class JUnitSuiteRunner(klass: java.lang.Class[T] forSome {type T <: Test}) extends org.junit.runner.Runner with Filterable with Sortable with TestDescription {
-  
+
   /**
    * aggregated test representing the whole test suite
    */
   var testSuite: Test = klass.newInstance
-  
+
   /**
    * runs the test suite by passing a JUnit4 RunNotifier which is wrapped in a JUnit3 TestListener to be able to run JUnit3 tests
    */
   override def run(notifier: RunNotifier) = {
-	val result= new TestResult();
-	result.addListener(createAdaptingListener(notifier));
-	testSuite.run(result);
+	  val result = new TestResult();
+	  result.addListener(createAdaptingListener(notifier));
+	  testSuite.run(result);
   }
 
   /**
@@ -42,7 +42,7 @@ class JUnitSuiteRunner(klass: java.lang.Class[T] forSome {type T <: Test}) exten
    * nothing to filter
    */
   def filter(filter: Filter) = {}
-  
+
   /**
    * nothing to sort
    */
@@ -62,10 +62,10 @@ trait TestDescription extends Classes {
    * not render properly as there can only be one test with a given in a given class.
    * For specs it is different as there can be the same name in 2 different systems (and those systems will be represented by the
    * same JUnit class: ExampleTestCase).
-   * 
+   *
    * This uses the createSuiteDescription method from JUnit as it is the only way to create a Description object having
    * the required name.
-   * 
+   *
    * @return the description of the test with a name including its hashcode
    */
   def asDescription(test: Test) = {
@@ -84,47 +84,47 @@ trait TestDescription extends Classes {
    * @return the description of the suite based on its name
    */
   def asDescription(ts: JUnitSuite) = createSuiteDescription(if (ts.getName == null) "" else ts.getName, null)
-  
+
   /**
    * create a Description from a TestCase or a JUnitSuite object
    */
   def makeDescription(test: Test): Description = {
-	if (test.isInstanceOf[JUnitSuite]) {
+	  if (test.isInstanceOf[JUnitSuite]) {
   	  val ts = test.asInstanceOf[JUnitSuite];
       val description= asDescription(ts)
       for (suite <- ts.suites)
-		description.addChild(makeDescription(suite))
+		    description.addChild(makeDescription(suite))
       for (t <- ts.testCases)
         description.addChild(makeDescription(t))
-	  description
-	} 
+	    description
+	  }
     else if (test.isInstanceOf[TestCase])
-	  asDescription(test.asInstanceOf[TestCase])
+	    asDescription(test.asInstanceOf[TestCase])
     else
-	  createSuiteDescription(test.getClass());
+	    createSuiteDescription(test.getClass());
   }
 }
 
 /**
- * This class listens for JUnit3 results (as a TestListener) and notifies a JUnit4 RunNotifier 
+ * This class listens for JUnit3 results (as a TestListener) and notifies a JUnit4 RunNotifier
  */
 class OldTestClassAdaptingListener(notifier: RunNotifier)  extends TestListener with TestDescription {
 
   /**
-   * Notifies the notifier of a test start 
+   * Notifies the notifier of a test start
    */
   def startTest(test: Test) = notifier.fireTestStarted(asDescription(test))
 
   /**
-   * Notifies the notifier of a test finish 
+   * Notifies the notifier of a test finish
    */
   def endTest(test: Test) = notifier.fireTestFinished(asDescription(test))
 
   /**
-   * Notifies the notifier of a new test failure (an error in JUnit3 is a Failure in JUnit4) 
+   * Notifies the notifier of a new test failure (an error in JUnit3 is a Failure in JUnit4)
    */
   def addError(test: Test, t: Throwable) = addNewFailure(test, t)
-	
+
   /**
    * Notifies the notifier of a test failure.
    * specs specificity: if the failure is a SkippedAssertionError, then notify of a skip
@@ -133,13 +133,11 @@ class OldTestClassAdaptingListener(notifier: RunNotifier)  extends TestListener 
     t match {
       // unfortunately the skip message can not be included for display in a description object
       // otherwise the description created when running the test and the description creating when
-      // parsing the whole suite for the first time will not match 
+      // parsing the whole suite for the first time will not match
       case skipped: SkippedAssertionError => notifier.fireTestIgnored(makeDescription(test))
       case _ => addNewFailure(test, t)
     }
   }
   private def addNewFailure(test: Test, t: Throwable) = notifier.fireTestFailure(new Failure(asDescription(test), t))
-
-
 }
 
