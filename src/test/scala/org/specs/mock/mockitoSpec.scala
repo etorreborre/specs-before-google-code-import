0ebdf4f5fc86bcf2d03610e9b1b48831f003b4f6
etorreborre <etorreborre@yahoo.com>
2009-03-12 23:58:41 +0900

    added the "was called" verification for mockito mocks

--- src/test/scala/org/specs/mock/mockitoSpec.scala
+++ src/test/scala/org/specs/mock/mockitoSpec.scala
@@ -13,36 +13,49 @@ h3. Let's verify some behaviour
 
 A mock is created with the @mock@ method:{"""
  //mock creation
- val mockedList = mock(List.class)
+ val mockedList = mock[List]
 
  // using mock object
  mockedList.add("one")
- mockedList.clear()
+ mockedList.clear()""" >@}
 
- //verification
- verify(mockedList).add("one")
- verify(mockedList).clear()""" >@}
-
-It is possible to check that some calls have been done on the mock.
- <ex>The @verify@ method is used to check a call</ex>{sample1IsOk}
+<ex>It is possible to check that some calls have been done on the mock with the @called@ matcher</ex>{"""
+  //verification
+  theMethod(mockedList).add("one") was called
+  theMethod(mockedList).clear() was called
+  """ >@}{sample1IsOk}
 
   </wiki> isSus
 }
-trait Mockito extends ExpectationsListener {
+trait Mockito extends ExpectableFactory {
   def mock[T](implicit m: scala.reflect.Manifest[T]): T = org.mockito.Mockito.mock(m.erasure).asInstanceOf[T]
-  def verifyMock[T](t: T): T = { try { org.mockito.Mockito.verify(t) } catch { case e => throw new FailureException(e.getMessage)} }
+  def theMethod[T](m: T) = org.mockito.Mockito.verify(m)
+  implicit def theCall(c: =>Any) = new CalledMock(c)
+  class CalledMock(c: =>Any) {
+    def was(callMatcher: CallMatcher) = theValue(c) must callMatcher
+  }
+  import org.specs.matcher._
+  import org.specs.matcher.MatcherUtils._
+  def called = new CallMatcher
+  class CallMatcher extends Matcher[Any] {
+    def apply(v: =>Any) = {
+      try { v } catch {
+    case e => (false, d(v) + " was called", d(v) + " was not called: " + e.getMessage)
+      }
+      (true, d(v) + " was called", d(v) + " was not called")
+    }
+  }
 }
 trait mockitoRules extends LiterateSpecification with Mockito with Wiki with Html with JUnit {
   override def htmlDir = "target"
   import org.mockito.Mockito._
-  case class Sample(s: () => Any) { def check = s() }
-  def sample(s: => Any) = Sample(() => s)
-  def sample1IsOk = sample {
-    val mockedList = mock[java.util.List[String]]
+  import org.mockito.Mockito
+  def sample1IsOk = eg {
+    import java.util.List
+    val mockedList = mock[List[String]]
     mockedList.add("one")
-//    mockedList.clear()
-    val m = verifyMock(mockedList)
-    m.add("one")
-    m.clear()
-  }.check
+    mockedList.clear()
+    theMethod(mockedList).add("one") was called
+    theMethod(mockedList).clear() was called
+  }
 }
