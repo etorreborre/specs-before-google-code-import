c7e7d4d1f409bbcb4d7c906b3ec6769304ed0ec2
etorreborre <etorreborre@yahoo.com>
2009-03-16 16:50:34 +0900

    added a mockito mocker class in the org.mockito package to access the MOCKING_PROGRESS variable

---
+++ src/main/scala/org/specs/mock/MockitoMocker.scala
@@ -0,0 +1,8 @@
+package org.mockito
+
+class MockitoMocker {
+  def mockingProgress = Mockito.MOCKING_PROGRESS
+  def mock[T](implicit m: scala.reflect.Manifest[T]): T = Mockito.mock(m.erasure).asInstanceOf[T]
+  def when[V](v: V) = Mockito.when(v)
+  def times(i: Int) = Mockito.times(i)
+}
