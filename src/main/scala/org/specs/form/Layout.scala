67c9b8cbc92c0622f4623a4bf7c39ee0a5f14e21
etorreborre <etorreborre@yahoo.com>
2009-03-27 19:41:41 +0900

    temporary fix for the max on firefox and IE. Needs to work on something more robust

--- src/main/scala/org/specs/form/Layout.scala
+++ src/main/scala/org/specs/form/Layout.scala
@@ -86,8 +86,9 @@ trait Layoutable extends ToHtml with IncludeExclude[ToHtml] { outerLayoutable =>
       case List(<th>{ b }</th>) => <th colspan={spanSize.toString}>{b}</th> % nodes.toList.first.attributes
       case List(<td>{ b }</td>) => <td colspan={spanSize.toString}>{b}</td> % nodes.toList.first.attributes
       case List(<td>{ b }</td>, Text(x)) => <td colspan={spanSize.toString}>{b}</td> % nodes.toList.first.attributes  ++ Text(x)
-      case <th>{ b }</th> :: otherThs => nodes.toList.first ++ updateLastTd(otherThs, spanSize)
-      case <td>{ b }</td> :: otherTds => nodes.toList.first ++ updateLastTd(otherTds, spanSize)
+      /* don't update the cell if it is bigger than the span size */
+      case <th>{ b }</th> :: otherThs if (nodes.toList.size < columnsNumber * 2) => nodes.toList.first ++ updateLastTd(otherThs, spanSize)
+      case <td>{ b }</td> :: otherTds if (nodes.toList.size < columnsNumber * 2) => nodes.toList.first ++ updateLastTd(otherTds, spanSize)
       case List(<table>{ x @ _*}</table>) => <table class="dataTable">{updateLastTd(x, spanSize)}</table>
       case <tr>{ y @ _*}</tr> :: otherRows => <tr>{updateLastTd(y, spanSize)}</tr> ++ updateLastTd(otherRows, spanSize)
       case Text(x) :: other => Text(x) ++ updateLastTd(other, spanSize)
