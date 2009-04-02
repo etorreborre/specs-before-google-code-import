476b5f2a40b128bf77bee7b1a1c945020315b98c
etorreborre <etorreborre@yahoo.com>
2009-04-02 09:31:48 +0900

    removed the -v option for the run class

--- src/main/scala/org/specs/runner/run.scala
+++ src/main/scala/org/specs/runner/run.scala
@@ -5,7 +5,7 @@ object run {
   def main(args: Array[String]) = {
     if (args.length == 0)
       println("The first argument should be the specification class name")
-    val spec = Classes.createObject[Specification](args(0), args.contains("-v"))
+    val spec = Classes.createObject[Specification](args(0), true)
     spec match {
       case Some(s) => s.main(args.drop(1))
       case None => println("The class " + args(0) + " could not be instantiated")
