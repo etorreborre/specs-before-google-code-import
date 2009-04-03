6f5ad8680dc0af9f4ce1c8822d6e192191933c43
etorreborre <etorreborre@yahoo.com>
2009-04-03 12:57:39 +0900

    added a 'super' specification with all traits

---
+++ src/main/scala/org/spex/Specification.scala
@@ -0,0 +1,7 @@
+package org.spex
+import org.specs.mock.Mockito
+import org.specs.Sugar
+import org.specs.SystemContexts
+import org.specs.runner._
+
+class Specification extends org.specs.Specification with Mockito with Sugar with SystemContexts with JUnit //with ScalaTest 
