7812389eae1c7f692be9b5a9d9588f7a59461607
etorreborre <etorreborre@yahoo.com>
2009-04-13 11:08:49 +0900

    added on more example on haveMatcherSpec

--- src/test/scala/org/specs/matcher/haveMatcherSpec.scala
+++ src/test/scala/org/specs/matcher/haveMatcherSpec.scala
@@ -8,6 +8,9 @@ class haveMatcherSpec extends Specification with JUnit { outer =>
     List("hello") must have size(1)
   }
   "A collection matcher starting with 'notHave' can be used with 'not have' as a separated words" in {
+    List("hello") must not have size(2)
+  }
+  "A collection matcher starting with 'notHave' can be used with 'not have' as a separated words" in {
     List("hello") must not have(size(2))
   }
   "A collection matcher starting with 'have' can be used with have as a separated word" in {
