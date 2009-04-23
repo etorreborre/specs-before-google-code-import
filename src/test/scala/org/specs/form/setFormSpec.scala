25791e5b7a7e2b177cf8b6949aed0c25b36df3a1
etorreborre <etorreborre@yahoo.com>
2009-04-23 16:48:41 +0900

    added missing license files

--- src/test/scala/org/specs/form/setFormSpec.scala
+++ src/test/scala/org/specs/form/setFormSpec.scala
@@ -1,3 +1,21 @@
+/**
+ * Copyright (c) 2007-2009 Eric Torreborre <etorreborre@yahoo.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software. Neither the name of specs nor the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written permission.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS INTHE SOFTWARE.
+ */
 package org.specs.form
 import org.specs.runner._
 
@@ -42,6 +60,17 @@ class setFormSpec extends org.specs.Specification with JUnit {
         tr(PersonLine("Bob",  42)) 
       }
       (form.execute.toXhtml \\("tr")).toList must have size 4
+    }
+    "not match rows twice" in {
+      val form = new SetForm(actual) {
+        th2("Name", "Age")
+        tr(PersonLine("Eric", 36)) 
+        tr(PersonLine("Eric", 36)) 
+      }
+      form.execute.matchedLines aka "matched lines" must have size 1
+      form.execute.matchedExpectedLines aka "matched expected lines" must have size 1
+      form.execute.matchedActual aka "matched actual lines" must have size 1 
+      form.execute.unmatchedExpectedLines aka "unmatched expected lines" must be empty
     }
   }
 }
