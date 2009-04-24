a4b31a6c5c6e068b8fdbb7d37228ed849fb5c017
etorreborre <etorreborre@yahoo.com>
2009-04-25 00:16:55 +0900

    fixed a failing example

--- src/test/scala/org/specs/util/matchingSpec.scala
+++ src/test/scala/org/specs/util/matchingSpec.scala
@@ -28,19 +28,19 @@ class matchingSpec extends spex.Specification {
   var edgeWeight = (t:(String, String)) => t._1.size + t._2.size 
   val sets = for {
     size1   <- Gen.choose(1, 3)
-    set1    <- Gen.vectorOf(size1, Gen.elements("Art", "Bill", "Chris"))
+    set1    <- Gen.vectorOf(size1, Gen.elements("Ar", "Bill", "Charles"))
     size2   <- Gen.choose(1, 3)
-    set2   <- Gen.vectorOf(size2, Gen.elements("Ann", "Bess", "Clara"))
+    set2   <- Gen.vectorOf(size2, Gen.elements("An", "Bess", "Claris"))
   } yield (set1, set2) 
 
   "matching an empty set with an empty set returns an empty list" in {
     bestMatch(List[String](), List[String](), edgeFunction, edgeWeight) must be empty
   }
-  "matching 2 non-empty sets must return edges with the maximum weigth" in {
+  "matching 2 non-empty sets must return edges with the maximum sum of weigths" in {
     sets must pass { s: (Seq[String], Seq[String]) => val (set1, set2) = s
-      val maxOfSet1 = set1.toList.maxElement((_:String).size).get
-      val maxOfSet2 = set2.toList.maxElement((_:String).size).get
-      bestMatch(set1, set2, edgeFunction, edgeWeight) must contain((maxOfSet1, maxOfSet2, (maxOfSet1, maxOfSet2)))
+      val maxOfSet1 = set1.toList.maximum((_:String).size)
+      val maxOfSet2 = set2.toList.maximum((_:String).size)
+      bestMatch(set1, set2, edgeFunction, edgeWeight).map(_._3).foldLeft(0)((total, t) => total + edgeWeight(t)) must >=(maxOfSet1 + maxOfSet2)
     }
   }
   "matching 2 non-empty sets must return a list of edges which size is the minimum size of both sets" in {
