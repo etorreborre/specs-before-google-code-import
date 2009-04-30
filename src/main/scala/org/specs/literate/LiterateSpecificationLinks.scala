8c8e4cf7633b34a03667b5a5a85989523ccb3f01
etorreborre <etorreborre@yahoo.com>
2009-05-01 00:36:53 +0900

    changed a package declaration to fix a compilation issue runner.Html instead of literate.Html

--- src/main/scala/org/specs/literate/LiterateSpecificationLinks.scala
+++ src/main/scala/org/specs/literate/LiterateSpecificationLinks.scala
@@ -18,7 +18,6 @@
  */
 package org.specs.literate
 import org.specs.specification._
-import org.specs.runner.Html
 /**
  * This trait allows to add links to other specifications inside a literate specification.
  * The link will be displayed as a Html link
@@ -27,8 +26,8 @@ import org.specs.runner.Html
  * to be reported when reporting the parent.
  */
 trait LiterateSpecificationLinks extends LinkedSpecification with Links { this: Specification => 
-  def linkTo(subSpec: Specification with Html): String = linkTo(subSpec.description, subSpec)
-  def linkTo(desc: String, subSpec: Specification with Html): String = {
+  def linkTo(subSpec: Specification with runner.Html): String = linkTo(subSpec.description, subSpec)
+  def linkTo(desc: String, subSpec: Specification with runner.Html): String = {
     super.linkTo(desc, subSpec)
     // execute the subSpec
     subSpec.failures
