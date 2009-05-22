4bb248c22d233551a84f7c334f2e57a9762b494d
etorreborre <etorreborre@yahoo.com>
2009-05-22 14:42:34 +1000

    added implicit defs for context properties

--- src/main/scala/org/specs/specification/ContextProperties.scala
+++ src/main/scala/org/specs/specification/ContextProperties.scala
@@ -4,6 +4,14 @@ import scala.collection.mutable.ListBuffer
 import org.specs.util.ReinitProperty
 
 trait ContextProperties extends BaseSpecification { this: BeforeAfter =>
+  implicit def toExamplePropertyBeforeExample[T](t: =>T) = new ExamplePropertyBeforeExample(t)
+  class ExamplePropertyBeforeExample[T](t: =>T) {
+    def beforeEx = exampleProp(t)
+  }
+  implicit def toSusPropertyBeforeExample[T](t: =>T) = new SusPropertyBeforeSus(t)
+  class SusPropertyBeforeSus[T](t: =>T) {
+    def beforeSus = susProp(t)
+  }
   private val exampleProperties = new ListBuffer[ReinitProperty[_]]
   private val susProperties = new ListBuffer[ReinitProperty[_]]
   def exampleProp[T](t: T) = { 
--- src/test/scala/org/specs/specification/contextPropertiesSpec.scala
+++ src/test/scala/org/specs/specification/contextPropertiesSpec.scala
@@ -5,7 +5,7 @@ class contextPropertiesSpec extends spex.Specification {
   "An example property in a Specification" should {
     "be resetted to its initial value between examples" in {
       object spec extends spex.Specification {
-        val i = exampleProp(0)
+        val i = 0.beforeEx
         "i is 0 in this example" in {
           i() must_== 0
           i(1)
@@ -20,7 +20,7 @@ class contextPropertiesSpec extends spex.Specification {
   "A system property in a Specification" should {
     "be resetted to its initial value system executions" in {
       object spec extends spex.Specification {
-        val i = susProp(0)
+        val i = 0.beforeSus
         "this system" should {
           "use i as a shared variable here" in {
             i() must_== 0
