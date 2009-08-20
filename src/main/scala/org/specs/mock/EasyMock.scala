82297c3c95035961e79a943288b23c4973aab0e2
etorreborre <etorreborre@yahoo.com>
2009-08-20 11:41:05 +1000

    added some comments

--- src/main/scala/org/specs/mock/EasyMock.scala
+++ src/main/scala/org/specs/mock/EasyMock.scala
@@ -1,3 +1,21 @@
+/**
+ * Copyright (c) 2007-2009 Eric Torreborre <etorreborre@yahoo.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software. Neither the name of specs nor the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written permission.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS INTHE SOFTWARE.
+ */
 package org.specs.mock
 
 import _root_.org.easymock.classextension.{ EasyMock => EasyClassMocker }
@@ -9,53 +27,68 @@ import org.specs.ExtendedThrowable._
 import java.lang.reflect.Proxy
 import org.easymock.internal._
 
-trait EasyMockLifeCycle extends LifeCycle with EasyMocks {
-  override def executeExpectations(ex: Examples, t: =>Any) = {
-    try { 
-      super.executeExpectations(ex, t)
-    }
-    catch {
-      case e if (e.getStackTrace.exists(_.toString.contains("easymock"))) => new FailureException(e.getMessage).throwWithStackTraceOf(e)
-    }
-  }
-  private def getControl(m: Object) = {
-     Proxy.getInvocationHandler(m).asInstanceOf[ObjectMethodsFilter].getDelegate.getControl
-  }
-}
-trait EasyMocks {
-  private[mock] val mocks = new scala.collection.mutable.ListBuffer[Object]
-}
-trait EasyMock extends ExpectationsListener with EasyMockLifeCycle with EasyMocks {
+/**
+ * This trait integrates EasyMock in specs so that mock exceptions are correctly 
+ * reported as specs failures and adds some syntactic sugar to allow an easier syntax.
+ */
+trait EasyMock extends ExpectationsListener with EasyMockLifeCycle {
+  /** 
+   * create a mock with a name
+   * @return the mock object
+   */
   def mockAs[T <: Object](name: String)(implicit m: scala.reflect.Manifest[T]): T = {
     val m1 = EasyClassMocker.createNiceMock(name, m.erasure).asInstanceOf[T]
     mocks.append(m1)
     m1
   }
+  /** 
+   * create a nice mock with a name
+\   * @return the mock object
+   */
   def niceMockAs[T <: Object](name: String)(implicit m: scala.reflect.Manifest[T]): T = {
     val m1 = EasyClassMocker.createNiceMock(name, m.erasure).asInstanceOf[T]
     mocks.append(m1)
     m1
   }
+  /** 
+   * create a strict mock with a name
+   * @return the mock object
+   */
   def strictMockAs[T <: Object](name: String)(implicit m: scala.reflect.Manifest[T]): T = {
     val m1 = EasyClassMocker.createStrictMock(name, m.erasure).asInstanceOf[T]
     mocks.append(m1)
     m1
   }
+  /** 
+   * create a mock
+   * @return the mock object
+   */
   def mock[T <: Object](implicit m: scala.reflect.Manifest[T]): T = {
     val m1 = EasyClassMocker.createMock(m.erasure).asInstanceOf[T]
     mocks.append(m1)
     m1
   }
+  /** 
+   * create a nice mock
+   * @return the mock object
+   */
   def niceMock[T <: Object](implicit m: scala.reflect.Manifest[T]): T = {
     val m1 = EasyClassMocker.createNiceMock(m.erasure).asInstanceOf[T]
     mocks.append(m1)
     m1
   }
+  /** 
+   * create a strict mock
+   * @return the mock object
+   */
   def strictMock[T <: Object](implicit m: scala.reflect.Manifest[T]): T = {
     val m1 = EasyClassMocker.createStrictMock(m.erasure).asInstanceOf[T]
     mocks.append(m1)
     m1
   }
+  /** 
+   * expect some calls and replay all mocks which have been previously declared
+   */
   def expect[T](t: =>T):T = {
     val result = t
     mocks.foreach { m => 
@@ -63,14 +96,11 @@ trait EasyMock extends ExpectationsListener with EasyMockLifeCycle with EasyMock
     }
     result.isExpectation
   }
-  /** @return an object supporting the stub methods. */
+  /** @return an object supporting the convienience methods on mock objects */
   implicit def theMock[T  <: Object](c: =>T) = new MockedObject(c)
-  /** @return an object supporting the stub methods. */
-  implicit def theCall[T](c: =>T) = new CalledObject(c)
-
   /** 
-   * This class provide stub methods like returns, throws and answers.
-   * Internally it calls EasyMock.expect(mock call).andReturn(returnValue)
+   * This class provide mock methods like reset, replay, verify, toNice,....
+   * Internally it calls the corresponding methods on EasyMocker
    */
   class MockedObject[T <: Object](c: =>T) {
     def toNice = EasyClassMocker.resetToNice(c)
@@ -86,6 +116,13 @@ trait EasyMock extends ExpectationsListener with EasyMockLifeCycle with EasyMock
     def makeThreadSafe = EasyClassMocker.makeThreadSafe(c, true)
     def dontMakeThreadSafe = EasyClassMocker.makeThreadSafe(c, false)
   }
+  /** @return an object supporting the convienience methods on calls. */
+  implicit def theCall[T](c: =>T) = new CalledObject(c)
+
+  /** 
+   * This class provide mock methods like returns, answers,....
+   * Internally it calls the corresponding methods on EasyMocker (andReturn, andAnswer,...)
+   */
   class CalledObject[T](c: =>T) {
     def returns(t: T) = EasyMocker.expect(c).andReturn(t)
     def stubReturns(t: T) = EasyMocker.expect(c).andStubReturn(t)
@@ -143,12 +180,19 @@ trait EasyMock extends ExpectationsListener with EasyMockLifeCycle with EasyMock
          }
        } 
      }
+     /**
+      * specific implementation of the Answer interface for a function taking no parameters
+      */
      class MockAnswer2[T](function: () => T) extends IAnswer[T] {
        def answer: T = function()
      }
   }
-  /** @return an object supporting the stub methods. */
+  /** @return an object supporting the chained expectations. */
   implicit def theExpectations[T](c: =>IExpectationSetters[T]) = new Expectations(c)
+  /** 
+   * This class provides a way to chain expectations on mocked calls with the same syntax as
+   * the one introduced by specs
+   */
   class Expectations[T](c: =>IExpectationSetters[T]) {
     def andReturns(t: T) = c.andReturn(t)
     def andStubReturns(t: T) = c.andStubReturn(t)
@@ -156,7 +200,13 @@ trait EasyMock extends ExpectationsListener with EasyMockLifeCycle with EasyMock
     def andDelegatesTo[S](other: S) = c.andDelegateTo(other)
     def andStubDelegatesTo[S](other: S) = c.andStubDelegateTo(other)
   }
+  /**
+   * replay one or several mocks 
+   */
   def replay[T <: Object](m: T*) = m.foreach(EasyClassMocker.replay(_))
+  /**
+   * verify one or several mocks. Each verify is counted as an expectation 
+   */
   def verify[T <: Object](m: T*) = {
     try {
       m.foreach(EasyClassMocker.verify(_).isExpectation)
@@ -165,3 +215,26 @@ trait EasyMock extends ExpectationsListener with EasyMockLifeCycle with EasyMock
     }
   }
 }
+/**
+ * This trait transforms EasyMock errors into specs failures
+ * It also cleans up the mocks variable used for the expect blocks.
+ */
+trait EasyMockLifeCycle extends LifeCycle {
+  /** list of mocks to replay in an expect block */
+  private[mock] val mocks = new scala.collection.mutable.ListBuffer[Object]
+
+  override def afterExample(ex: Examples) = {
+    mocks.clear
+    super.afterExample(ex)
+  }
+  
+  override def executeExpectations(ex: Examples, t: =>Any) = {
+    try { 
+      super.executeExpectations(ex, t)
+    }
+    catch {
+      case e if (e.getStackTrace.exists(_.toString.contains("easymock"))) => new FailureException(e.getMessage).throwWithStackTraceOf(e)
+    }
+  }
+}
+
