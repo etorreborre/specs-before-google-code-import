a040ccd0929c220f14115c62b910e7c0a78ebfc0
etorreborre <etorreborre@yahoo.com>
2009-08-25 08:38:47 +1000

    added comments to the LifeCycle traits

--- src/main/scala/org/specs/specification/ExampleLifeCycle.scala
+++ src/main/scala/org/specs/specification/ExampleLifeCycle.scala
@@ -23,53 +23,99 @@ import org.specs.ExtendedThrowable._
 
 /**
  * This trait models the execution cycle of an example.
+ * 
+ * LifeCycles can be chained with a "parent" relationship, where the parent LifeCycle of an example is his 
+ * enclosing sus and the parent LifeCycle of a Sus is its enclosing specification.
+ * 
+ * Since a Specification mixes in this trait, some methods can be overriden to provide more specialized behaviour:
+ * <ul>
+ * <li>before(/after)Example: code to execute before(/after) the example</li>
+ * <li>before(/after)Expectations: code to execute before(/after) the example expectations. The difference with beforeExample is that 
+ * any FailureException thrown by this method will be counted as an example failure (whereas it is counted as an exception in beforeExample)</li>
+ * <li>executeExpectations: execute the example expectations</li>
+ * <li>executeExample: strategy for executing the example. The default strategy is implemented in the ExampleLifeCycle trait. 
+ * In this trait, we check if the example is the first one to execute. If so, it is executed right away. If not, the execution is 
+ * delegated to the parent LifeCycle which ultimately is the SpecificationExecutor trait which executes the example in
+ * isolation from other examples by executing it in a clone of the Specification.
+ * </li>
+ * </ul>
+ *  
  */
 trait LifeCycle {
   /** lifecycles can be chained via parent-child relationships */
   private[specs] var parent: Option[LifeCycle] = None
-  /** current example defining the context of this execution */
+  /** 
+   * current list of examples defining the context of this execution. Every example created during by the executeExpectations method
+   * will be attached to this list
+   */
   private[specs] var current: Option[Examples] = None
   /** a predicate which will decide if an example must be re-executed */
   private[specs] var untilPredicate: Option[() => Boolean] = None
   /** this variable defines if examples should be executed as soon as defined */
   private[specs] protected var sequential = false
+  /** @return true if if examples should be executed as soon as defined  */
   def isSequential = sequential
   /** this variable defines if examples should be executed as soon as defined */
   def setSequential() = sequential = true
-  
-  def withCurrent(ex: Examples)(a: => Any) = {
+  /** execute a block of code with a specific list of examples as the container to use for examples created by the block */
+  private[specs] def withCurrent(ex: Examples)(a: => Any) = {
     val c = current.orElse(parent.flatMap(_.current))
     setCurrent(Some(ex))
     try { a } finally {
       setCurrent(c)
     }
   }
-  def setCurrent(ex: Option[Examples]): Unit = {
+  /** set an example as the current example for this lifecycle and its parent */
+  private[specs] def setCurrent(ex: Option[Examples]): Unit = {
     current = ex
     parent.map(_.setCurrent(ex))
   }
-  /** forwards the call to the "parent" cycle */
+  /** @return true if there is an until predicate, and if it is true */
   def until: Boolean = parent.map(_.until).getOrElse(true) && untilPredicate.getOrElse(() => true)()
-  def afterExample(ex: Examples) = {}
+  /** define the actions to be done before an example is executed */
   def beforeExample(ex: Examples) = {}
-  /** forward the call to the "parent" cycle */
+  /** define the actions to be done after an example has been executed */
+  def afterExample(ex: Examples) = {}
+  /** define actions to be done just before the example expectations, as if they were part of the example */
   def beforeExpectations(ex: Examples): Unit = parent.map(_.beforeExpectations(ex))
-  def executeExpectations(ex: Examples, t: =>Any): Any = parent.map(_.executeExpectations(ex, t)).getOrElse(t)
-  /** forward the call to the "parent" cycle */
+  /** define actions to be done just after the example expectations, as if they were part of the example */
   def afterExpectations(ex: Examples): Unit = parent.map(_.afterExpectations(ex))
+  /** define how to execute the example expectations */
+  def executeExpectations(ex: Examples, t: =>Any): Any = parent.map(_.executeExpectations(ex, t)).getOrElse(t)
+  /** define how to execute the example itself. This method has to be overriden by subtraits */
   def executeExample(ex: Examples): this.type = this
 }
-
+/**
+ * Implementation of a LifeCycle backed up by an ExampleStructure which can
+ * be added any subexample created during the Example execution.
+ * 
+ * It will also check that the executed example contains expectations,
+ * otherwise will mark the example as "PENDING"
+ */
 trait ExampleLifeCycle extends LifeCycle with ExampleStructure {
-  var execution: Option[ExampleExecution] = None
-  def executed = execution.map(_.executed).getOrElse(true)
+  /** 
+   * object containing the method for executing the example expectations, including when and how
+   * the LifeCycle methods should be called.
+   */
+  private[specs] var execution: Option[ExampleExecution] = None
+  /** @return true if the execution has been executed */
+  private[specs] def executed = execution.map(_.executed).getOrElse(true)
+  /** abstract method (defined in Example) executing the example itself */
   def executeThis: Unit
+  /** 
+   * executing the expectations of an example contained by this LifeCycle, 
+   * skipping the example if it doesn't contain any expectations 
+   */
   override def executeExpectations(ex: Examples, t: =>Any): Any = {
     val executed = super.executeExpectations(ex, t)
     skipIfNoExpectations()
     executed
   }
-  /** execute one sub example */
+  /** 
+   * execute one sub example either right away if this is the first example
+   * of this list of example, otherwise the execution is delegated to the parent lifecycle
+   * for isolated execution
+   */
   override def executeExample(ex: Examples): this.type = { 
     if (!exampleList.isEmpty  && exampleList.head == ex)
       ex.executeThis
@@ -77,19 +123,25 @@ trait ExampleLifeCycle extends LifeCycle with ExampleStructure {
       parent.map(_.executeExample(ex)) // forward the execution strategy to the parent 
     this
   }
+  /**
+   * copy the execution results from another example.
+   * This method is used when an example has been executed in isolation in another spec.
+   */
   def copyExecutionResults(other: Examples) = {
     copyFrom(other)
     execution.map(_.executed = true)
   }
-
+  /**
+   * @throw a skipped exception if there are no expectations in that example. This behaviour can be overriden by setting the
+   * "examplesWithoutExpectationsMustBePending" property in the Configuration object
+   */
   protected def skipIfNoExpectations() = {
     if (thisExpectationsNumber == 0 && 
-          exampleList.isEmpty && thisSkipped.isEmpty && thisFailures.isEmpty && thisErrors.isEmpty && 
-          Configuration.config.examplesWithoutExpectationsMustBePending)
+        exampleList.isEmpty && thisSkipped.isEmpty && thisFailures.isEmpty && thisErrors.isEmpty && 
+        Configuration.config.examplesWithoutExpectationsMustBePending)
       throw new SkippedException("PENDING: not yet implemented").removeTracesAsFarAsNameMatches("(specification.Example|LiterateSpecification)")
   }
-
-  /** reset in order to be able to run the example again */
+  /** reset in order to be able to run the example again. This is only used for testing */
   override def resetForExecution: this.type = {
     super.resetForExecution
     execution.map(_.resetForExecution)
@@ -101,7 +153,9 @@ trait ExampleLifeCycle extends LifeCycle with ExampleStructure {
 object DefaultLifeCycle extends Example("default life cycle")
 
 /**
- * This class encapsulates the execution of an example
+ * This class encapsulates the execution of an example.
+ * It will check if the example is tagged for execution ("accepted") and it will execute the expectations as
+ * long as the untilr predicate is not satisfied (if there is one)
  */
 class ExampleExecution(var example: Examples, val expectations: () => Any) {
   /** function containing the expectations to be run */
@@ -115,6 +169,15 @@ class ExampleExecution(var example: Examples, val expectations: () => Any) {
 
   /** flag used to memorize if the example has already been executed once. In that case, it will not be re-executed */
   private[specification] var executed = false
+  /** 
+   * execution of the example expectations. The example lifecycle methods are used here:<ul>
+   * <li>beforeExample: add an error and skip the rest if an exception is thrown</li>
+   * <li>beforeExpectations</li>
+   * <li>executeExpectations</li>
+   * <li>afterExpectations</li>
+   * <li>afterExample: add an error if an exception is thrown</li>
+   * <ul/>
+   */
   val execution = () => {
     var failed = false
     // try the "before" methods. If there is an exception, add an error and return the current example
@@ -152,5 +215,6 @@ class ExampleExecution(var example: Examples, val expectations: () => Any) {
       executed = true
     }
   }
+  /** reset the execution so that a new call to execute will indeed execute the expectations again */
   def resetForExecution = executed = false
 }
