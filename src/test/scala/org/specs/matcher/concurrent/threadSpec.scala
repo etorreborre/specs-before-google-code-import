978a611b33bef792912bd6d01987cae51d552e34
etorreborre <etorreborre@yahoo.com>
2009-08-28 08:14:32 +1000

    fixed the example which was missing a lock

--- src/test/scala/org/specs/matcher/concurrent/threadSpec.scala
+++ src/test/scala/org/specs/matcher/concurrent/threadSpec.scala
@@ -6,13 +6,15 @@ class threadSpec extends org.specs.Specification with org.specs.matcher.Threads 
   import PimpedReadWriteLock._
 
   "this system" should {
-    thread("writer thread") {
-      waitForBeat(1)
-      "the beat must advance" in { beat must_== 1 }
-    }
     thread("reader thread") { 
       lock.read { 
         waitForBeat(2) 
+      }
+    }
+    thread("writer thread") {
+      waitForBeat(1)
+      lock.write { 
+        "the beat must advance" in { beat must_== 2 }
       }
     }
   }
