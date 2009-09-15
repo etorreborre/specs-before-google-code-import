6c26b05dc74473c61ea07e453fe6e71a802fd301
etorreborre <etorreborre@yahoo.com>
2009-09-15 22:53:50 +1000

    protected against null values

--- src/main/scala/org/specs/matcher/AnyMatchers.scala
+++ src/main/scala/org/specs/matcher/AnyMatchers.scala
@@ -91,8 +91,9 @@ trait AnyBaseMatchers {
         qx = qx + ": " + getClassName(x)
       }
       import org.specs.Products._
+      def isNull(a: Any) = a match { case (ref: AnyRef) => ref eq null; case _ => false }
       val failureMessage = details match {
-        case full: fullDetails if (full.startDiffSize <= x.toString.size) => {
+        case full: fullDetails if (!isNull(x) && full.startDiffSize <= x.toString.size) => {
           EditMatrix(dy, qx).showDistance(full.separators, full.shortenSize).toList.mkString(" is not equal to ")
         }
         case _ => dy + " is not equal to " + qx
