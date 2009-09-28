2c5fb818cf81477993eba93d42f588ea2f8d87b1
etorreborre <etorreborre@yahoo.com>
2009-09-29 09:14:22 +1000

    better error messages when an instance of a specific class can't be created

--- src/main/scala/org/specs/runner/SpecificationsFinder.scala
+++ src/main/scala/org/specs/runner/SpecificationsFinder.scala
@@ -21,7 +21,7 @@ package org.specs.runner
 import org.specs.io.FileSystem
 import java.util.regex._
 import scala.collection.mutable.Queue
-import org.specs.util.Classes._
+import org.specs.util.Classes
 
 /**
  * Companion SpecsFinder object to create a SpecsFinder returning an aggregate Specification of
@@ -58,7 +58,7 @@ case class SpecsFinder(path: String, pattern: String, asOneSpecification: Boolea
 /**
  * This trait browses files on a given path and returns what can be matching specification class names.
  */
-trait SpecificationsFinder extends FileSystem {
+trait SpecificationsFinder extends FileSystem with Classes {
 
    /**
     * @param path a path to a directory containing scala files (it can be a glob: i.e. "dir/**/*spec.scala")
@@ -107,4 +107,10 @@ trait SpecificationsFinder extends FileSystem {
    * @return None in case of an exception.
    */
   def createSpecification(className: String): Option[Specification] = tryToCreateObject[Specification](className)
+  /**
+   * @return a <code>Specification</code> object from a className if that class is a <code>Specification</code> class.<br>
+   * Tries to load the class name and cast it to a specification
+   * @return None in case of an exception.
+   */
+  def createSpecification(className: String, printMessage: Boolean, printStackTrace: Boolean): Option[Specification] = createObject[Specification](className, printMessage, printStackTrace)
 }
