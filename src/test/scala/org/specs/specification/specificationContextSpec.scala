91ad45426b183e4c9884b2d5e2ced66c1137a116
etorreborre <etorreborre@yahoo.com>
2009-10-02 16:36:37 +1000

    sligth change of name to avoid mvn test failure

--- src/test/scala/org/specs/specification/specificationContextSpec.scala
+++ src/test/scala/org/specs/specification/specificationContextSpec.scala
@@ -4,7 +4,7 @@ import org.specs._
 class specificationContextSpec extends SpecificationWithJUnit {
   "A specification context" can {
     "be used to specify the actions before the specification" in {
-      val s = new TestedSpec { 
+      val s = new TestedSpecification { 
         new SpecContext { beforeSpec(out.append("beforeSpec")) } 
         "this system" should { "have one example" in { 1 must_== 1 } }    
       }
@@ -15,6 +15,6 @@ class specificationContextSpec extends SpecificationWithJUnit {
 }
 import org.specs.io.mock._
 
-class TestedSpec extends Specification with MockOutput {
+class TestedSpecification extends Specification with MockOutput {
   val out = new scala.collection.mutable.ListBuffer[String]
 }
