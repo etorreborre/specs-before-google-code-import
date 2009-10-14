7d3bd458b4d732dbfc333ae871e23091f74b2401
etorreborre <etorreborre@yahoo.com>
2009-10-15 09:09:52 +1100

    added better paragraph operators

--- src/test/scala/org/specs/literate/documentSpec.scala
+++ src/test/scala/org/specs/literate/documentSpec.scala
@@ -33,6 +33,7 @@ trait DocumentsFactory { this: BaseSpecification =>
     }
     }</md>
   }
+  def br = new LineBreak
   class LineBreak extends Document {
     def toXhtml = <br></br>
   }
@@ -42,20 +43,28 @@ trait DocumentsFactory { this: BaseSpecification =>
   class DocSequence(documents: List[Document]) extends Document {
     def toXhtml = reduce(documents, { (d: Document) => d.toXhtml })
   }
+  class Decorator(document: Document, decorator: (NodeSeq => NodeSeq)) extends Document {
+    def toXhtml = decorator(document.toXhtml)
+  }
+  class Paragraph(document: Document) extends Decorator(document, (d: NodeSeq) => <p>{d}</p>)
+  
   abstract class Document {
     if (currentDocument == None) currentDocument = Some(this)
-    def \\(d: Document): Document = this append d
-    def \\(s: =>String): Document = this append s.m
-    def \(d: Document): Document = this plus d
-    def \(s: =>String): Document = this plus s.m
-    def append(d: Document): Document = {
-      currentDocument = Some(new DocSequence(List(this, new LineBreak, d)))
+    def \\(d: Document): Document = parAddPar(d)
+    def \\(s: =>String): Document = \\(s.m)
+    def \:(d: Document): Document = add(new Paragraph(d), this)
+    def :\(d: Document): Document = addPar(d)
+    def \(d: Document): Document = plus(d)
+    def \(s: =>String): Document = \(s.m)
+    
+    def parAddPar(d: Document): Document = add(new Paragraph(this), new Paragraph(d))
+    def addPar(d: Document): Document = add(this, new Paragraph(d))
+    def parAdd(d: Document): Document = add(new Paragraph(this), d)
+    def add(d1: Document, d2: Document) = {
+      currentDocument = Some(new DocSequence(List(d1, d2)))
       currentDocument.get
     }
-    def plus(d: Document): Document = {
-      currentDocument = Some(new DocSequence(List(this, d)))
-      currentDocument.get
-    }
+    def plus(d: Document): Document = add(this, d)
     def toXhtml: NodeSeq
     def toLiterateDesc: LiterateDescription = LiterateDescription(<div>{ toXhtml }</div>)
   }
@@ -66,7 +75,7 @@ import scala.xml._
 class DocumentSample extends LiterateSpecification with Documents with org.specs.runner.Html {
 
   """A document is a piece of text with _markup_ notation""" \\
-  """It allows to compose a whole literate specification from small elements""" \\
+  """It allows to compose a whole literate specification from small elements""" \
   ("and examples too" in { 1 must_== 1 })
 
 }
@@ -86,24 +95,35 @@ class DocumentSpec extends org.spex.Specification with DocumentsFactory {
       docString must include("<em>markup</em>")
     }
     "have a \\\\ operator to append a document" in {
-      docString must include("notation</md><br></br><md>It")
+      docString must include("notation</md></p><p><md>It")
     }
     "have a \\ operator to inline an example" in {
       doc.asHtml(doc) must \\("ex")
+    }
+    "allow break returns between docs" in {
+      ("hello" \: br :\  "world" toXhtml).toString must include("hello</md></p><br></br><p><md>world")
     }
   }
   def docString = docBody toString
   def docBody = doc.asHtml(doc) \\ "body" 
     
+  "A Document" should {
+    "have a \\ method to add another document" in {
+      ("hello" \ "world" toXhtml).toString must_== "<md>hello</md><md>world</md>" 
+    }
+    "have a \\\\ method to create 2 paragraphs and add them" in {
+      ("hello" \\ "world" toXhtml).toString must_== "<p><md>hello</md></p><p><md>world</md></p>" 
+    }
+    "have a :\\ method to create a paragraph for the second doc and add it to the first" in {
+      ("hello".m :\ "world" toXhtml).toString must_== "<md>hello</md><p><md>world</md></p>" 
+    }
+    "have a \\: method to create a paragraph for the first doc and add it to the second" in {
+      ("hello" \: "world" toXhtml).toString must_== "<p><md>hello</md></p><md>world</md>" 
+    }
+  }
   "A Markdown document" should {
     "have a toXhtml method displaying the document" in {
       "hello".m.toXhtml.toString must_== "<md>hello</md>" 
-    }
-    "have a \\ method to add another document" in {
-      ("hello" \ "world") .toXhtml.toString must_== "<md>hello</md><md>world</md>" 
-    }
-    "have a \\\\ method to add another paragraph" in {
-      ("hello" \\ "world") .toXhtml.toString must_== "<md>hello</md><br></br><md>world</md>" 
     }
   }
   "A document sequence" should {
