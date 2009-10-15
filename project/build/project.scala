860a44c84c9d9b02f588d39b7cd366a47d2b7bbe
etorreborre <etorreborre@yahoo.com>
2009-10-16 10:26:57 +1100

    fixed the sbt project file so that it compiles out of the box

--- project/build/project.scala
+++ project/build/project.scala
@@ -11,8 +11,8 @@ class Project(info: ProjectInfo) extends DefaultProject(info) {
   val wikitext 		= "org.eclipse.mylyn.wikitext" % "wikitext" % "0.9.4.I20090220-1600-e3x" 
   val wikitextile 	= "org.eclipse.mylyn.wikitext" % "wikitext.textile" % "0.9.4.I20090220-1600-e3x" 
   val markdown 		= "org.markdownj" % "markdownj" % "0.3.0-1.0.2b4" 
-  val scalatest 	= "org.scalatest" % "scalatest" % "1.0" 
-  val scalacheck 	= "org.scala-tools.testing" % "scalacheck" % "1.6" 
+  val scalatest 	= "org.scalatest" % "scalatest" % "0.9.5" 
+  val scalacheck 	= "org.scala-tools.testing" % "scalacheck" % "1.6-SNAPSHOT" 
   val jmock 		= "org.jmock" % "jmock" % "2.4.0" 
   val easymock 		= "org.easymock" % "easymock" % "2.5.1" 
   val easymockclass	= "org.easymock" % "easymockclassextension" % "2.4" 
@@ -20,4 +20,9 @@ class Project(info: ProjectInfo) extends DefaultProject(info) {
   val cglib 		= "cglib" % "cglib" % "2.1_3"  
   val cglibnodep	= "cglib" % "cglib-nodep" % "2.1_3"  
   val objenesis 	= "org.objenesis" % "objenesis" % "1.0"
+
+  override def managedStyle = ManagedStyle.Maven
+  val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/snapshots/"
+  Credentials(Path.userHome / ".ivy2" / ".credentials", log)
+
 }
