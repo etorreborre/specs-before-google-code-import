b75a8ee2da7ebbae6b16f94f7f686982b58da323
etorreborre <etorreborre@yahoo.com>
2009-10-21 08:58:05 +1100

    refactored the reporter spec by using subspecifications

--- src/test/scala/org/specs/runner/reporterPlanSpec.scala
+++ src/test/scala/org/specs/runner/reporterPlanSpec.scala
@@ -11,32 +11,21 @@ class reporterPlanSpec extends Specification with Sugar {
       help must containMatch("only display") and containMatch("without executing examples")
     }
   }
-  "A console reporter with the -plan option, when reporting the specification" should {
-    "not execute examples, thus show 0 expectations" in {
-      plan must containMatch("0 expectation")
-    }
-    "display the sus descriptions" in {
-      plan must containMatch("it")
-    }
-    "display the first level examples" in {
-      plan must containMatch("do this")
-    }
-    "not display the second level examples" in {
-      plan must not containMatch("subex1")
-    }
-  }
-  "A xml reporter with the -plan option, when reporting the specification" should {
-    "not execute examples, thus show 0 expectations" in {
-      xmlPlan must not be matching("expectations=\"[1-9]\"")
-    }
-    "display the sus descriptions" in {
-      xmlPlan must include("it")
-    }
-    "display the first level examples" in {
-      xmlPlan must include("do this")
-    }
-    "not display the second level examples" in {
-      xmlPlan must not include("subex1")
+  include(ReporterPlan("console reporter", consoleReporter), ReporterPlan("xml reporter", xmlReporter))
+  case class ReporterPlan(n: String, reporter: {def plan: String; def expectations: String}) extends Specification(n) with Sugar {
+    "A "+n+" with the -plan option, when reporting the specification" should {
+      "not execute examples, thus show 0 expectations" in {
+        reporter.plan must not be matching(reporter.expectations)
+      }
+      "display the sus descriptions" in {
+        reporter.plan must include("it")
+      }
+      "display the first level examples" in {
+        reporter.plan must include("do this")
+      }
+      "not display the second level examples" in {
+        reporter.plan must not include("subex1")
+      }
     }
   }
   val s = new TestSpecification {
@@ -51,28 +40,28 @@ class reporterPlanSpec extends Specification with Sugar {
       
     }
   } 
-    
   class TestSpecification extends org.specs.Specification with MockOutput {
     def help = displayHelp
+  }
+  object consoleReporter {
+    def plan: String = {
+      s.args = Array("-plan")
+      s.reportSpecs
+      s.messages.toList.mkString("\n")
+    }
+    def expectations: String = "[1-9] expectation"
+  }
+  val xml = new XmlRunner(s, "target") with MockFileSystem with MockOutput
+  object xmlReporter {
+    def plan: String = { 
+      xml.args = Array("-plan")
+      xml.reportSpecs
+      xml.files.values.next
+    }
+    def expectations = "expectations=\"[1-9]\""
   }
   def help = {
     s.help
     s.messages.toList
   }
-  def plan = {
-    s.args = Array("-plan")
-    executeSpec
-  }
-  def executeSpec = {
-    s.reportSpecs
-    s.messages.toList
-  }
-  val reporter = new XmlRunner(s, "target") with MockFileSystem with MockOutput
-  def xmlPlan: String = { 
-   reporter.args = Array("-plan")
-   reporter.reportSpecs
-   reporter.files.values.next
-  }
 }
-
-
