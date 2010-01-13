15a11484b5506ac7438c041269a409693d99f871
etorreborre <etorreborre@yahoo.com>
2010-01-13 21:56:50 +1100

    adding a missing brace

--- src/main/scala/org/specs/runner/NotifierRunner.scala
+++ src/main/scala/org/specs/runner/NotifierRunner.scala
@@ -85,6 +85,7 @@ class NotifierRunner(val specifications: Array[Specification], val notifiers: Ar
         system.ownSkipped.foreach { skipped =>
           notifier.systemSkipped(skipped.getMessage)
         }
+      }
     for (example <- system.examples)
       reportExample(example)
     notifiers.foreach { _.systemCompleted(system.header) }
