b5cd3ec937b21bbbf565739ff7b8694d163a9595
etorreborre <etorreborre@yahoo.com>
2010-02-03 00:19:49 +1100

    fixed the compilation issue

--- src/test/scala/org/specs/matcher/haveMatcherSpec.scala
+++ src/test/scala/org/specs/matcher/haveMatcherSpec.scala
@@ -38,38 +38,38 @@ class haveMatcherSpec extends SpecificationWithJUnit { outer =>
     List(1) must not have(size(2))
   }
   
-  def eventually[T] = new EventuallyMatcher[T]
-  class EventuallyMatcher[T] extends Matcher[T] { 
+  def possibly[T] = new PossiblyMatcher[T]
+  class PossiblyMatcher[T] extends Matcher[T] { 
     def apply(v: =>T) = (true, "", "")
     def ? (m: Matcher[T]) = m
   }
-  implicit def toEventuallyMatcherDecorator[T](m: Matcher[T]) = new EventuallyMatcherDecorator(m)
-  class EventuallyMatcherDecorator[T](m: Matcher[T]) extends Matcher[T] { 
-    def apply(v: =>T) = m(eventualMatch(v))
-    def eventualMatch(v: =>T) = v
-    def eventually = this
+  implicit def toPossiblyMatcherDecorator[T](m: Matcher[T]) = new PossiblyMatcherDecorator(m)
+  class PossiblyMatcherDecorator[T](m: Matcher[T]) extends Matcher[T] { 
+    def apply(v: =>T) = m(possibleMatch(v))
+    def possibleMatch(v: =>T) = v
+    def possibly = this
   }
-  implicit def toEventuallyMatchable[T](m: Matcher[T]) = new EventuallyMatchable(m)
-  class EventuallyMatchable[T](m: Matcher[T]) { 
-    def :: (e: EventuallyMatcher[Nothing]) = m // add the eventually logic here
+  implicit def toPossiblyMatchable[T](m: Matcher[T]) = new PossiblyMatchable(m)
+  class PossiblyMatchable[T](m: Matcher[T]) { 
+    def :: (e: PossiblyMatcher[Nothing]) = m // add the possibly logic here
   }
-  implicit def toEventuallyMatcherResult[T](result: Result[T]) = new EventuallyMatcherResult(result)
-  class EventuallyMatcherResult[T](result: Result[T]) {
-    def be_==(a: T) = result.matchWith(eventually(outer.be_==(a)))
-    def be(a: T) = result.matchWith(eventually(outer.be(a)))
-    private def eventually(m: Matcher[T]) = m  // add some logic for the semantics of "eventually"
+  implicit def toPossiblyMatcherResult[T](result: Result[T]) = new PossiblyMatcherResult(result)
+  class PossiblyMatcherResult[T](result: Result[T]) {
+    def be_==(a: T) = result.matchWith(possibly(outer.be_==(a)))
+    def be(a: T) = result.matchWith(possibly(outer.be(a)))
+    private def possibly(m: Matcher[T]) = m  // add some logic for the semantics of "possibly"
   }    
 
   "An extension for a matcher can be created" in {
-    "A string" must eventually be_== "A string"
+    "A string" must possibly be_== "A string"
   }
   "An extension for a matcher can be created" in {
-    "A string" must be_==("A string").eventually
+    "A string" must be_==("A string").possibly
   }
   "An extension for a matcher can be created" in {
-    "A string" must eventually :: be_==("A string")
+    "A string" must possibly :: be_==("A string")
   }
   "An extension for a matcher can be created" in {
-    "A string" must eventually ? be_==("A string")
+    "A string" must possibly ? be_==("A string")
   }
 }
