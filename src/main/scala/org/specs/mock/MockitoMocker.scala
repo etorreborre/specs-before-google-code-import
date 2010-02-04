c19918a5d9264d59f92d8de9550f3f7741831280
etorreborre <etorreborre@yahoo.com>
2010-02-04 22:35:34 +1100

    updated the files license

--- src/main/scala/org/specs/mock/MockitoMocker.scala
+++ src/main/scala/org/specs/mock/MockitoMocker.scala
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2007-2009 Eric Torreborre <etorreborre@yahoo.com>
+ * Copyright (c) 2007-2010 Eric Torreborre <etorreborre@yahoo.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
  * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
