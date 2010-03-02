9c2bc6d049f84af83eca92acf7eb76a56829af79
etorreborre <etorreborre@yahoo.com>
2010-03-02 20:23:38 +1100

    fixed the compilation issue

--- src/test/scala/org/specs/runner/testInterfaceRunnerSpec.scala
+++ src/test/scala/org/specs/runner/testInterfaceRunnerSpec.scala
@@ -109,7 +109,7 @@ class issue111Specification extends Specification with MockOutput {
     "an example" in { 1 must_== 1 }
   }
 }
-
+import org.specs._
 object sbtLiterateSpecification extends HtmlSpecification with MockOutput with MockFileSystem {
  "this" is <t>
    A literate specification with an example { 1 must_== 1 }
