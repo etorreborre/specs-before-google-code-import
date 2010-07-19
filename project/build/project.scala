4b3195e4413460a7da85831f0c5dd4b8c84160a2
etorreborre <etorreborre@yahoo.com>
2010-07-19 13:10:12 -0700

    small modifications for the sbt project

--- project/build/project.scala
+++ project/build/project.scala
@@ -2,12 +2,14 @@ import sbt._
 
 class Project(info: ProjectInfo) extends DefaultProject(info) {
   val mavenLocal = "Local Maven Repository" at "file://D:/mvn-repository"
-  override def outputDirectoryName = "build"
+
+  override def outputDirectoryName = "target"
   override def managedDependencyPath = "project" / "lib_managed"
   override def compileOptions = Unchecked :: super.compileOptions.toList
   override def javaCompileOptions = JavaCompileOption("-Xmx256m -Xms64m -Xss1M") :: Nil
   override def testJavaCompileOptions = JavaCompileOption("-Xmx256m -Xms64m") :: Nil
   override def includeTest(s: String) = { s.endsWith("Spec") || s.endsWith("Unit") }
+
   val junit 		= "junit" % "junit" % "4.5"
   val wikitext 		= "org.eclipse.mylyn.wikitext" % "wikitext" % "0.9.4.I20090220-1600-e3x" 
   val wikitextile 	= "org.eclipse.mylyn.wikitext" % "wikitext.textile" % "0.9.4.I20090220-1600-e3x" 
