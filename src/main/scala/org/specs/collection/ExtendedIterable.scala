cd642c6d3acf31395b1d32cd3eab54e5cd374737
etorreborre <etorreborre@yahoo.com>
2010-08-02 21:00:55 +1000

    started the refactoring of ExtendedIterables - it's compiling,...

--- src/main/scala/org/specs/collection/ExtendedIterable.scala
+++ src/main/scala/org/specs/collection/ExtendedIterable.scala
@@ -17,34 +17,22 @@
  * DEALINGS IN THE SOFTWARE.
  */
 package org.specs.collection
-import scala.collection.mutable.ListBuffer
-import org.specs.collection.ExtendedList._
 
 /**
- * The ExtendedIterable object offers utility methods applicable to iterables like:<ul>
- * <li><code>toStream</code>
- * <li><code>toDeepString</code>: calls toString recursively on the iterable elements
- * <li><code>sameElementsAs</code>: compares 2 iterables recursively
+ * The ExtendedIterable trait offers utility methods applicable to iterable like:
+ * * `toDeepString`: calls toString recursively on the iterable elements
+ * * `sameElementsAs`: compares 2 iterables recursively
  * </ul>
  */
-object ExtendedIterable {
+private[specs] trait ExtendedIterable {
   /**
    * implicit definition to transform an iterable to an ExtendedIterable
    */
-  implicit def iterableToExtended[A](xs : Iterable[A]) = new ExtendedIterable(xs)
-
+  private[specs] implicit def iterableToExtended[A](xs: Iterable[A]) = new ExtendedIterable(xs)
   /**
-   * See the description of the ExtendedIterable object
+   * See the description of the ExtendedIterable trait
    */
-  class ExtendedIterable[A](xs:Iterable[A]) {
-    /**
-     * @return a Stream created from the iterable
-     */
-    def toStream = xs.iterator.toStream
-    /**
-     * alias for any type of Iterable
-     */
-    type anyIterable = Iterable[T] forSome { type T }
+  private[specs] class ExtendedIterable[T](xs: Iterable[T]) {
     /**
      * @return the representation of the elements of the iterable using the toString method recursively
      */
@@ -52,17 +40,15 @@ object ExtendedIterable {
       if (!xs.isEmpty && xs == xs.iterator.next)
         xs.toString
       else
-        "[" + xs.toList.map { x => 
-		    x match {
-			  case i: Iterable[_] => i.toDeepString
-			  case _ => x.toString
-			}
-        }.mkString(", ") + "]"
+        xs.map { 
+		  case i: Iterable[_] => i.toDeepString
+		  case x => x.toString
+		}.mkString("[" , ", ", "]")
     }
     /**
      * @return true if the 2 iterables contain the same elements, in the same order, according to a function f
      */
-    def isSimilar[B >: A](that: Iterable[B], f: Function2[A, B, Boolean]): Boolean = {
+    def isSimilar[S >: T](that: Iterable[S], f: Function2[T, S, Boolean]): Boolean = {
       val ita = xs.iterator
       val itb = that.iterator
       var res = true
@@ -72,9 +58,18 @@ object ExtendedIterable {
       !ita.hasNext && !itb.hasNext && res
     }
     /**
+     * remove the first element satifying the predicate
+     * @return a list minus the first element satisfying the predicate
+     */
+    def removeFirst(predicate: T => Boolean): Iterable[T] = {
+	  if (xs.isEmpty) xs
+	  else if (predicate(xs.head)) xs.drop(1)
+	  else xs.take(1) ++ xs.removeFirst(predicate)
+    }
+    /**
      * @return true if the second iterable elements are contained in the first, in order
      */
-    def containsInOrder[A](l: Iterable[A]): Boolean = {
+    def containsInOrder(l: Iterable[T]): Boolean = {
       val firstList = xs.toList
       val secondList = l.toList
       (firstList, secondList) match {
@@ -92,45 +87,41 @@ object ExtendedIterable {
     /**
      * @return true if the 2 iterables contain the same elements recursively, in any order
      */
-    def sameElementsAs(that: Iterable[A]): Boolean = sameElementsAs(that, (x, y) => x == y)
+    def sameElementsAs(that: Iterable[T]): Boolean = sameElementsAs(that, (x, y) => x == y)
     /**
-     * @return true if the 2 iterables contain the same elements (according to a comparision function f) recursively, in any order
+     * @return true if the 2 iterables contain the same elements 
+     *         (according to a comparison function f) recursively, in any order
      */
-    def sameElementsAs(that: Iterable[A], f: (A, A) => Boolean): Boolean = {
-      def isNotItsOwnIterable(a: Iterable[_]) = a.isEmpty || a.iterator.next != a
-	  def matchTwo(x: A, y: A): Boolean = {
+    def sameElementsAs(ys: Iterable[T], f: (T, T) => Boolean): Boolean = {
+      def isItsOwnIterable(a: Iterable[_]) = !a.isEmpty && a.iterator.next == a
+	
+	  def isSame(x: T, y: T): Boolean = {
 		(x, y) match {
-		  case (a: Iterable[_], b:Iterable[_]) if (isNotItsOwnIterable(a)) => x.asInstanceOf[Iterable[A]].sameElementsAs(y.asInstanceOf[Iterable[A]], f)
+		  case (a: Iterable[_], b:Iterable[_]) if (!isItsOwnIterable(a)) => a.asInstanceOf[Iterable[T]].sameElementsAs(b.asInstanceOf[Iterable[T]], f)
 		  case _ => f(x, y)
 		}
 	  }
-      val ita = xs.iterator.toList
-      val itb = that.iterator.toList
-      var res = true
-      (ita, itb) match {
-        case (Nil, Nil) => true
-        case (a: Iterable[_], b: Iterable[_]) => {
-          if (a.headOption.isDefined && b.headOption.isDefined) {
-            val (x, y, resta, restb) = (a.head, b.head, a.drop(1), b.drop(1))
-            matchTwo(x, y) && resta.sameElementsAs(restb, f) ||
-            resta.exists(matchTwo(_, y)) && restb.exists(matchTwo(_, x)) &&
-              resta.removeFirst(matchTwo(_, y)).sameElementsAs(restb.removeFirst(matchTwo(_, x)), f)
-          }
-          else
-            false
-        }
-        case _ => ita == itb
-      }
+      def sameElements(xs: Iterable[T], ys: Iterable[T]) = {
+		val (x, y, resta, restb) = (xs.head, ys.head, xs.drop(1), ys.drop(1))
+		  
+		isSame(x, y) && resta.sameElementsAs(restb, f) ||
+		  resta.exists(isSame(_, y)) && 
+		  restb.exists(isSame(_, x)) &&
+		  resta.removeFirst(isSame(_, y)).sameElementsAs(restb.removeFirst(isSame(_, x)), f)
+	  }
+      xs.isEmpty && xs.isEmpty ||
+      !xs.isEmpty && !xs.isEmpty && sameElements(xs, ys)
     }
     /**
      * adds the sameElementsAs method to any object in order to do that comparison recursively
      */
-    implicit def anyToSameElements(x: Any) = new AnyWithSameElements(x)
+    private[specs] implicit def anyToSameElements(x: Any): AnyWithSameElements = new AnyWithSameElements(x)
     /**
      * Class adding the <code>sameElementsAs</code> method to any object. The default implementation uses standard equality (==)
      */
-    class AnyWithSameElements(x: Any) {
+    private[specs] class AnyWithSameElements(x: Any) {
       def sameElementsAs(that: Any): Boolean = x == that
     }
   }
 }
+object ExtendedIterable extends ExtendedIterable
